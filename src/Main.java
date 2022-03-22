57001d90c18f66e57bf49155756aa341bc3eaea8
vallifanov <vallifanov@gmail.com>
2022-03-22 19:10:05 +1200

    игнор и коммит

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+//        Входные данные
+        int amount = 14750;
+        int one_mile = 20;
+//        Логика процесса
+        int bonus = amount / one_mile;
+                System.out.println(bonus);
+    }
+}
